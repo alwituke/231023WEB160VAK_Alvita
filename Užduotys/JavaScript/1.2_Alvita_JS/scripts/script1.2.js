fd58a3c4b54ea62391e5b9848600579d20c7a5bc
alwituke <alwituke@gmail.com>
2023-11-14 18:31:08 +0200

    1.2 JS užduotis

---
+++ Užduotys/JavaScript/1.2_Alvita_JS/scripts/script1.2.js
@@ -0,0 +1,8 @@
+
+// pritaikom salyga kai norim isvesti pirmus 10 skaičių kurie dalinasi is 3 ir 5
+for (let x = 1; x<=150; x++) {
+
+    if (x % 3 === 0 && x % 5 === 0) {
+        console.log(x)
+    }
+}

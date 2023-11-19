e559003983bdb5f3bb3a946c25bfdb42bcb1c318
alwituke <alwituke@gmail.com>
2023-11-20 00:50:37 +0200

    1.5 užduotis

---
+++ Užduotys/JavaScript/1.5_Alvita_JS/scripts/script1.5.js
@@ -0,0 +1,9 @@
+
+let vaisiai = ["Obuolys", "Bananas", "Kriaušė"];
+console.log(vaisiai);
+
+vaisiai.unshift("Apelsinas"); 
+console.log(vaisiai);
+
+vaisiai.push("Melionas"); 
+console.log(vaisiai);

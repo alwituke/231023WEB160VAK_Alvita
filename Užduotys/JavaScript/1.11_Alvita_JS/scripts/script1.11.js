a8c95a268ccb373a14da10091dd150e8ce2f20da
alwituke <alwituke@gmail.com>
2023-11-20 20:57:19 +0200

    1.11 uzduotis

---
+++ Užduotys/JavaScript/1.11_Alvita_JS/scripts/script1.11.js
@@ -0,0 +1,36 @@
+
+//
+let darbuotojas = [
+    {
+        vardas: "Jonas",
+        pozicija: "vadybininkas",
+        atlyginimas: 1500
+    },
+    {
+        vardas: "Onute",
+        pozicija: "buhaltere",
+        atlyginimas: 1800
+    },
+    {
+        vardas: "Petras",
+        pozicija: "direktorius",
+        atlyginimas: 2000
+    },
+];
+
+darbuotojas.map(function(darbuotojas) {
+    darbuotojas.buvesAtlyginimas = darbuotojas.atlyginimas;
+    console.log(Math.random()*20+11);
+    let atlyginimoKoeficientas = Math.floor(Math.random()*20+11);
+    console.log(atlyginimoKoeficientas);
+
+    darbuotojas.atlyginimas = darbuotojas.buvesAtlyginimas + darbuotojas.buvesAtlyginimas * atlyginimoKoeficientas/100;
+
+    darbuotojas.algosPalyginimas = function (){
+        console.log(`Darbuotojo ${this.vardas} atlyginimas padidėjo nuo ${this.buvesAtlyginimas} iki ${this.atlyginimas} (${((this.atlyginimas-this.buvesAtlyginimas)/this.buvesAtlyginimas)*100}%)`);
+    }
+return darbuotojas;
+});
+
+
+darbuotojas[0].algosPalyginimas();

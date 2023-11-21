f0811773c63ec10230759f3f28f4e64d001dbe21
alwituke <alwituke@gmail.com>
2023-11-21 21:03:49 +0200

    1.17 uzduotis

---
+++ Užduotys/JavaScript/1.17_Alvita_JS/scripts/script1.17.js
@@ -0,0 +1,13 @@
+
+
+let picsumPhoto = document.querySelector("img");
+
+console.log(picsumPhoto);
+
+picsumPhoto.classList.add("pic");
+picsumPhoto.setAttribute(`id`, `header`);
+picsumPhoto.setAttribute(`name`, `paveikslelis`);
+picsumPhoto.setAttribute(`src`, `https://picsum.photos/200/200`);
+picsumPhoto.setAttribute(`alt`, `paveikslelio aprašymas`);
+
+

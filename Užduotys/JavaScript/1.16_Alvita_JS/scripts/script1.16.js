d0811c09381ef3e9c7c7a72765b8d6401250d226
alwituke <alwituke@gmail.com>
2023-11-21 21:36:57 +0200

    1.16 uzduotis

--- Užduotys/JavaScript/1.16_Alvita_JS/scripts/script1.16.js
+++ Užduotys/JavaScript/1.16_Alvita_JS/scripts/script1.16.js
@@ -6,14 +6,14 @@ let div2 = document.getElementsByTagName("div")[1];
 
 let div3 = document.getElementsByTagName("div")[2];
 
-div1.innerText = "Naujas antrastes tekstas";
-div2.innerHTML = "Naujas paragrafo tekstas";
-div3.textContent = "Naujas porastes tekstas";
+div1.innerText = "Naujas &ltantrastes&gt tekstas";
+div2.innerHTML = "Naujas &ltparagrafo&gt tekstas";
+div3.textContent = "Naujas &ltporastes&gt tekstas";
 
 console.log(div1);
 console.log(div2);
 console.log(div3);
 
-div1.innerText = "<b>Labas</b>";
-div2.innerHTML = "<b>Labas</b>";
-div3.textContent = "<b>Labas</b>";
+div1.innerText = "<b>Labas</b>, kuo tu vardu?";
+div2.innerHTML = "<b>Labas</b>, kuo tu vardu?";
+div3.textContent = "<b>Labas</b>, kuo tu vardu?";

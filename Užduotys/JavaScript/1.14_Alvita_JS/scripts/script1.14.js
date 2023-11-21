0ed1eca325a4f2c6fe6bddf34414c25741e9b283
alwituke <alwituke@gmail.com>
2023-11-21 21:37:21 +0200

    1.14 uzduoties papildymas

--- Užduotys/JavaScript/1.14_Alvita_JS/scripts/script1.14.js
+++ Užduotys/JavaScript/1.14_Alvita_JS/scripts/script1.14.js
@@ -4,6 +4,19 @@ let divai = document.getElementsByTagName("div");
 
 let paragrafai = document.getElementsByTagName("p");
 
+
+//dėstytojo variantas:
+// reikia keisti html į jc --> array
+Array.from(divai).forEach(function(divas){
+    console.log(divas);
+});
+
+// 2 var
+for (let paragrafas of paragrafai) {
+    console.log(paragrafas)
+}
+
+
 // visus kartu
 // for (let x = 0; x < 5; x++){
 
@@ -12,12 +25,15 @@ let paragrafai = document.getElementsByTagName("p");
 
 // atskirai divaiir atskirai paragrafai
 for (let x = 0; x < 5; x++){
+//universalesnis - tinka pasikeitus elementų skaičiui
+//for (let x = 0; x < divai.length; x++){
 
     console.log(divai[x]);
     }
 
 for (let y = 0; y < 5; y++){
-
+//universalesnis - tinka pasikeitus elementų skaičiui
+//for (let y = 0; y < paragrafai.length; y++){
         console.log(paragrafai[y]);
         }
 
@@ -27,8 +43,35 @@ paragrafai[4].classList.add("klase");
 let elementasSuKlasemis = document.getElementsByClassName ("klase");
 
 for (let x = 0; x < 2; x++){
+//universalesnis - tinka pasikeitus elementų skaičiui
+//for (let x = 0; x < elementasSuKlasemis.length; x++){
 
     console.log(elementasSuKlasemis[x]);
     }
     
-// ar galima sukurti nauja elementa is dvieju skirtingas klases turinciu elementu?
+// ar galima sukurti nauja kintamąjį iš dviejų skirtingas klases turinčių elementų?
+
+divai[3].classList.add("klase1");
+paragrafai[2].classList.add("klase2");
+
+//let elementaiSuKlasemis1 = document.getElementsByClassName ("klase1");
+//elementaiSuKlasemis1.concat(document.getElementsByClassName ("klase2"));
+
+//arba 
+// let elementaiSuKlasemis1 = document.getElementsByClassName("klase1").concat(document.getElementsByClassName ("klase2")); 
+
+// html paverčia į jc suprantamą masyvą
+let elementaiSuKlasemis1 = Array.from(document.getElementsByClassName("klase1")).concat(Array.from((document.getElementsByClassName("klase2"))));
+
+console.log(elementaiSuKlasemis1);
+
+
+for (let x = 0; x < 2; x++){
+
+    console.log(elementaiSuKlasemis1[x]);
+    }
+
+
+// kt var
+let elementaiSuKlasemis2 = document.querySelectorAll(".klase1, .klase2")
+console.log(elementaiSuKlasemis2);

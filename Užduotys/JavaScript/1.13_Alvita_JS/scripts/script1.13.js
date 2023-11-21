e9bd58fc754fd96230edc3a74f636aaf6e29def6
alwituke <alwituke@gmail.com>
2023-11-21 12:17:49 +0200

    1.13 uzduotis

---
+++ Užduotys/JavaScript/1.13_Alvita_JS/scripts/script1.13.js
@@ -0,0 +1,9 @@
+const antraste = document.getElementById("antrastes-id");
+const antraste2 = document.getElementById("antrastes2-id");
+const paragrafas = document.getElementById("paragrafo-id");
+const paragrafas2 = document.getElementById("paragrafo2-id");
+
+console.log(antraste);
+console.log(antraste2);
+console.log(paragrafas);
+console.log(paragrafas2);

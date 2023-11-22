7817c5dca630a6fd17148b51541ecb61a3f5815d
alwituke <alwituke@gmail.com>
2023-11-22 16:21:34 +0200

    1.6 uzduotis

---
+++ Užduotys/JavaScript/1.6_Alvita_JS/scripts/script1.6.js
@@ -0,0 +1,11 @@
+
+
+let numAvys= 4;
+let kiekMenSpausdinti = 12;
+let numMenuo = 1;
+
+while (numMenuo <= kiekMenSpausdinti ) {
+    numAvys = numAvys * 4;
+    console.log(`Po ${numMenuo} mėn. bus ${numAvys} avių.`);
+    numMenuo++;
+}
